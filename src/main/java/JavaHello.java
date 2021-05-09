80cd20226ed87302e3c837d18d041fa2990d978c
胡花欣 <huhuaxin@huhuaxindeAir.lan>
2021-05-10 01:23:40 +0800

    code commit

---
+++ src/main/java/JavaHello.java
@@ -0,0 +1,7 @@
+public class JavaHello {
+    public static void main(String args[]){
+        System.out.println("hello world！");
+        System.out.println("I like this !");
+        System.out.println("Third row");
+    }
+}
---
+++ src/main/java/javabackup.java
@@ -0,0 +1,7 @@
+public class javabackup {
+    public static void main(String args[]){
+        System.out.println("hello world！");
+        System.out.println("I like this !");
+        System.out.println("Third row");
+    }
+}
